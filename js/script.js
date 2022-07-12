63452a43e601834e2138e65b6bb6ac24352cf669
Yuriy Klimenko <yurriyklimenko@gmail.com>
2022-07-12 18:52:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let screenValue = document.getElementById("value");
+let nmbBtns = document.querySelectorAll(".nmb-btn");
+let mathBtns = document.querySelectorAll(".math-btn");
+
+let equalsBtn = document.getElementById("btn-equals");
+let clearBtn = document.getElementById("btn-clear");
+let dotBtn = document.getElementById("btn-dot");
+let memory = 0;
+screenValue.innerHTML = "0";
+
+//набор цифр
+  nmbBtns.forEach(function (btn) {
+    // Вешаем событие клик
+    btn.addEventListener("click", function () {
+      if (screenValue.innerHTML == "0") {
+        screenValue.innerHTML = this.innerHTML;
+      } else {
+        screenValue.innerHTML += this.innerHTML;
+      }
+    });
+  });
+
+dotBtn.addEventListener("click", function () {
+  if (!screenValue.innerHTML.includes(".")) {
+    screenValue.innerHTML += this.innerHTML;
+  }
+});
+
+clearBtn.addEventListener("click", function () {
+  screenValue.innerHTML = "0";
+});
+
+equalsBtn.addEventListener("click", function() {
+   let result = screenValue.innerHTML;
+   result = eval(screenValue.innerHTML);
+   screenValue.innerHTML = result;
+})
+
